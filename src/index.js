0afd30013a3e517e124ca9ca91ac5eff94d10747
Kylory <72569852+Kylory@users.noreply.github.com>
2021-05-26 21:44:20 +0300

    added fetch and render functions

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import '../node_modules/material-design-icons/iconfont/material-icons.css';
 // material - design - icons / iconfont / material - icons.css;
 import './css/style.css';
+import galleryTpl from './templates/galleryTpl.hbs';
 // import fetchCountriesByName from './js/fetchCountriesByName';
 // import renderCountryCard from './js/renderCountryCard';
 // import _ from 'lodash';
 // import clearMarkup from './js/clearMarkup';
 
-// const refs = {
-//   input: document.querySelector('.input'),
-// };
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
 
 // function showCountries() {
 //   if (refs.input.value) {
@@ -17,3 +18,29 @@ import './css/style.css';
 // }
 
 // refs.input.addEventListener('input', _.debounce(showCountries, 500));
+
+const API_KEY = '21804857-e4d02e1e62ab2bb6123c0439f';
+
+function fetchImagesByName(searchQuery) {
+  return fetch(
+    `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=12&key=${API_KEY}`,
+  ).then(response => {
+    return response.json();
+  });
+}
+function someMagic(responseFromApi) {
+  return responseFromApi.hits;
+}
+
+function createGalleryCardsMarkup(item) {
+  return galleryTpl(item);
+}
+
+function renderGalleryCard(markup) {
+  refs.gallery.innerHTML = markup;
+}
+
+fetchImagesByName('cats')
+  .then(obj => someMagic(obj))
+  .then(hit => createGalleryCardsMarkup(hit))
+  .then(markup => renderGalleryCard(markup));
