20e35275ff8323b2472a90492579e99e7b41ef7f
Kyrylo <krllmsk@gmail.com>
2021-05-26 09:31:36 +0300

    added files

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
-// import './css/style.css';
+import '../node_modules/material-design-icons/iconfont/material-icons.css';
+// material - design - icons / iconfont / material - icons.css;
+import './css/style.css';
 // import fetchCountriesByName from './js/fetchCountriesByName';
 // import renderCountryCard from './js/renderCountryCard';
 // import _ from 'lodash';
