7bb10d91f000abffaf99eb18e0093d74bde2e6c2
Kyrylo <krllmsk@gmail.com>
2021-05-27 09:27:15 +0300

    added search and render

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ import './css/style.css';
 import galleryTpl from './templates/galleryTpl.hbs';
 // import fetchCountriesByName from './js/fetchCountriesByName';
 // import renderCountryCard from './js/renderCountryCard';
-// import _ from 'lodash';
+import _ from 'lodash';
 // import clearMarkup from './js/clearMarkup';
 
 const refs = {
   gallery: document.querySelector('.gallery'),
+  loadMoreBtn: document.querySelector('.load-more-btn'),
+  searchQuery: document.querySelector('.search-input'),
 };
+
+// let searchQuery = refs.searchQuery.value;
 
 // function showCountries() {
 //   if (refs.input.value) {
@@ -17,30 +21,32 @@ const refs = {
 //   } else clearMarkup();
 // }
 
-// refs.input.addEventListener('input', _.debounce(showCountries, 500));
-
 const API_KEY = '21804857-e4d02e1e62ab2bb6123c0439f';
 
-function fetchImagesByName(searchQuery) {
+function fetchImagesByName(searchQuery, page) {
   return fetch(
-    `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=1&per_page=12&key=${API_KEY}`,
+    `https://pixabay.com/api/?image_type=photo&orientation=horizontal&q=${searchQuery}&page=${page}&per_page=12&key=${API_KEY}`,
   ).then(response => {
     return response.json();
   });
 }
-function someMagic(responseFromApi) {
-  return responseFromApi.hits;
-}
 
-function createGalleryCardsMarkup(item) {
-  return galleryTpl(item);
+function createGalleryCardsMarkup(responseFromApi) {
+  const hits = responseFromApi.hits;
+  return galleryTpl(hits);
 }
 
 function renderGalleryCard(markup) {
-  refs.gallery.innerHTML = markup;
+  refs.gallery.insertAdjacentHTML('beforeend', markup);
 }
 
-fetchImagesByName('cats')
-  .then(obj => someMagic(obj))
-  .then(hit => createGalleryCardsMarkup(hit))
-  .then(markup => renderGalleryCard(markup));
+function searchImageByName() {
+  let searchQuery = refs.searchQuery.value;
+  //   let page = 1;
+  fetchImagesByName(searchQuery)
+    .then(hit => createGalleryCardsMarkup(hit))
+    .then(markup => renderGalleryCard(markup));
+  //   page += 1;
+}
+
+refs.searchQuery.addEventListener('input', _.debounce(searchImageByName, 600));
